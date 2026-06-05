b5eba16be01954fe07cf1805f87666a43f081fce
Connor Clark <c.clark000@hotmail.com>
2026-06-05 10:09:00 -0500

    add bufferline

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,7 @@
+vim.pack.add {
+  { src = 'https://github.com/akinsho/bufferline.nvim', tag = '*' },
+  'https://github.com/nvim-tree/nvim-web-devicons',
+}
+vim.opt.termguicolors = true
+require('bufferline').setup{}
+
